f7cdea38a2cee0185cb4eb6cc0c526a035e1866e
unixengineer <unixengineer@gmail.com>
2018-01-09 17:59:12 -0800

    Cleanup

--- build.sh
+++ build.sh
@@ -3,16 +3,16 @@
 # Workspace env would be set by Jenkins
 export PATH=${WORKSPACE}/venv/bin:/usr/local/bin:${PATH}
 if [ ! -d "venv" ]; then
-       # if you want a fresh build everytime, delete and create a new virtualenv
-       # rmvirtualenv venv
         virtualenv venv
 fi
+
+# if you want a fresh build everytime, delete and create a new virtualenv
+# rmvirtualenv venv
+
+# Activate this virtualenv
 . venv/bin/activate
 
 pip install -r requirements.txt --download-cache=/tmp/${JOB_NAME} --upgrade
 
 ## Lets now check what version shows up
 aws --version
-
-
-
