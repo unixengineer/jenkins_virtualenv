0c7288b13321064b16e07197b5d33768d2d2df2e
Ramprasad Prabhakar <ram.prasad@elliemae.com>
2018-01-09 17:55:46 -0800

    this time we update

--- build.sh
+++ build.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-# WORKSPACE=~/Code
-export PATH=$WORKSPACE/venv/bin:/usr/local/bin:$PATH
+
+# Workspace env would be set by Jenkins
+export PATH=${WORKSPACE}/venv/bin:/usr/local/bin:${PATH}
 if [ ! -d "venv" ]; then
        # if you want a fresh build everytime, delete and create a new virtualenv
        # rmvirtualenv venv
