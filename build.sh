45e69b68efc5b56bc23a49bc5372504e683e86a3
Ramprasad Prabhakar <ram.prasad@elliemae.com>
2018-01-09 17:32:50 -0800

    testing pip

--- build.sh
+++ build.sh
@@ -2,9 +2,12 @@
 # WORKSPACE=~/Code
 export PATH=$WORKSPACE/venv/bin:/usr/local/bin:$PATH
 if [ ! -d "venv" ]; then
+       # if you want a fresh build everytime, delete and create a new virtualenv
+       # rmvirtualenv venv
         virtualenv venv
 fi
 . venv/bin/activate
 
-pip install -r requirements.txt --download-cache=/tmp/$JOB_NAME
+pip install -r requirements.txt --download-cache=/tmp/${JOB_NAME}
+
 
